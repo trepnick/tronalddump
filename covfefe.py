0be73205078ddfaa1d9a42a269d88b6975ed3a9a
Ben Haines <bfhain@gmail.com>
2020-05-08 16:07:57 -0400

    add basic documentation

--- covfefe.py
+++ covfefe.py
@@ -13,6 +13,13 @@ ADVICE_URL = "https://api.adviceslip.com"
 @click.option("-c", is_flag=True)
 @click.option("-a", is_flag=True)
 def cli(t, c, a):
+    """The primary CLI for the APIs
+
+    Arguments:
+        t {flag} -- supplied on command line - indicates to get random trump quote
+        c {flag} -- supplied on command line - indicates to get random chuck norris joke
+        a {flag} -- supplied on command line - indicates to get random piece of advice
+    """
     if t:
         click.echo(get_random_trump_quote())
     elif c:
@@ -24,16 +31,31 @@ def cli(t, c, a):
 
 
 def get_random_trump_quote():
+    """Gets a random donald trump quote from the tronalddump API
+
+    Returns:
+        str -- the returned quote
+    """
     r = requests.get(TRONALD_DUMP_URL + "/random/quote")
     return r.json()["value"]
 
 
 def get_random_chuck_fact():
+    """gets a random chuck norris joke
+
+    Returns:
+        str -- the returned joke
+    """
     r = requests.get(CHUCK_NORRIS_URL + "/jokes/random")
     return r.json()["value"]
 
 
 def get_random_advice():
+    """gets a random piece of advice
+
+    Returns:
+        str -- the returned advice
+    """
     r = requests.get(ADVICE_URL + "/advice")
     return r.json()["slip"]["advice"]
 
